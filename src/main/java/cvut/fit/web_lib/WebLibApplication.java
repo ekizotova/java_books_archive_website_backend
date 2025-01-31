91f01c4da9a96350861fad0c5db4d7549cd62fc2
Ekaterina <ekaterina.izotova.work@gmail.com>
2025-01-31 23:51:06 +0100

    Create WebLibApplication.java

---
+++ src/main/java/cvut/fit/web_lib/WebLibApplication.java
@@ -0,0 +1,13 @@
+package cvut.fit.web_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLibApplication.class, args);
+	}
+
+}
