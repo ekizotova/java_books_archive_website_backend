95aa9bad61ac9e86cb637e48c80c150a5439f74a
Ekaterina <ekaterina.izotova.work@gmail.com>
2025-02-04 22:23:57 +0100

    Create WebLibApplicationTests.java

---
+++ src/test/java/cvut/fit/web_lib/WebLibApplicationTests.java
@@ -0,0 +1,13 @@
+package cvut.fit.web_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
